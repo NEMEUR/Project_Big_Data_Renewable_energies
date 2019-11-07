d88b5d692dfc39686038976bd6f9734bddec3c0e
Olivier B <o.brunet@outlook.com>
2019-11-07 10:03:19 +0100

    reorganize folders

---
+++ 02_organisation_docker_github/github.sh
@@ -0,0 +1,9 @@
+# at the very beginning, JUST NEEDED ONE TIME
+# create a specific folder for the project and clone the existing content on github
+mkdir Projet
+cd Projet
+git clone https://github.com/obrunet/Project_Big_Data_Renewable_energies.git
+cd Project_Big_Data_Renewable_energies/
+
+
+
